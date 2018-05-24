66091efc645e262897d8a6ad28389d378e6ff23b
L.B <40447035S@ntnu.edu.tw>
2018-05-24 14:32:28 +0800

    moving S2VT folder to GP
    all files

---
+++ move_dataset_to_folder.py
@@ -0,0 +1,6 @@
+import os
+ftrain = open("E:\pythonwork\Video_to_sequence\data\data_set\MSR_Video_Description_Corpus.train.csv",'w+')
+fvalidation = open("E:\pythonwork\Video_to_sequence\data\data_set\MSR_Video_Description_Corpus.validation.csv",'w+')
+ftest = open("E:\pythonwork\Video_to_sequence\data\data_set\MSR_Video_Description_Corpus.test.csv",'w+')
+
+os.system("move 
